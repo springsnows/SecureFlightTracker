b5972f8a9d45c0feff65346c63365117eabe70ef
springsnows <145714082+springsnows@users.noreply.github.com>
2025-03-28 11:00:41 +0000

    Fixes for text two speech

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SecureFlightTracker
+
+
+Matthias Schäfer, Martin Strohmeier, Vincent Lenders, Ivan Martinovic and Matthias Wilhelm.
+"Bringing Up OpenSky: A Large-scale ADS-B Sensor Network for Research".
+In Proceedings of the 13th IEEE/ACM International Symposium on Information Processing in Sensor Networks (IPSN), pages 83-94, April 2014.
--- tests/text2speech.py
+++ tests/text2speech.py
@@ -1,17 +1,12 @@
 import pyttsx3
 
 
-text = ("Hello World!")
+text = ("Hello World!") # example text to be spoken
 
 def text2speech(text):
     engine = pyttsx3.init()
-    voices = engine.getProperty('voices')       #getting details of current voice
-    #engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
-    engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
-    engine.setProperty('rate', 90) 
+    engine.setProperty('rate', 110) # audio speed
     engine.say(text)
     engine.runAndWait()
-
-
 
 text2speech(text)
